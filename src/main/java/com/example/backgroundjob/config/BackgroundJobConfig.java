cf656c5a7caf0fe91f82b04d0a1e7a87b262f371
Asif Hajiyev <asif.haciyev@kapitalbank.az>
2021-06-23 16:58:28 +0400

    changing component to configuration

--- src/main/java/com/example/backgroundjob/config/BackgroundJobConfig.java
+++ src/main/java/com/example/backgroundjob/config/BackgroundJobConfig.java
@@ -1,12 +1,12 @@
 package com.example.backgroundjob.config;
 
 import org.jobrunr.jobs.mappers.JobMapper;
-import org.springframework.context.annotation.Bean;
 import org.jobrunr.storage.InMemoryStorageProvider;
 import org.jobrunr.storage.StorageProvider;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 public class BackgroundJobConfig {
     @Bean
     public StorageProvider storageProvider(JobMapper jobMapper) {
